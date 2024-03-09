f1a115b0a58be75959d8b25f75a6787a40470c2f
Jairik <jairikmc@gmail.com>
2024-03-09 12:56:23 -0500

    Added some functions and stuff

--- Project01/SortTimer.cpp
+++ Project01/SortTimer.cpp
@@ -30,27 +30,43 @@ int getNumArrays();
 /*Tetrieve and validate input for the number of elements in each array, in which is returned 
 via an int pointer */
 int *getSizes(int);
-//Return a pointer to an array of randomly generated integers, given size and bounds
-int *getRandomArray(int, int);
+//Return a pointer to an array of randomly generated integers, given size
+int *getRandomArray(int);
 //Start the chrono timer and return the start time
-auto startTimer();
+chrono::time_point<std::chrono::high_resolution_clock> startTimer();
 //Stop the chrono timer and return time elasped, given start as parameter
-auto endTimer(double);
+long endTimer(chrono::time_point<std::chrono::high_resolution_clock>);
 //Helper function to clean up and simplify main, write the given output to the ofstream
 void logTime(ofstream);
 
 
 int main() {
     int *currentArray; //Will hold the current array of random integers
-    ofstream outputFile("SortTimes.csv"); //Open the file to output the data into
     cout << "Welcome to JJ's Sort Timer Program!" << endl; //Introduction message
     int numArrays = getNumArrays(); //Get the number of arrays and assign to numArrays
     int *arraySizes = getSizes(numArrays); //Hold the size of each array
+    int size; //Used 
+
+    ofstream outputFile("SortTimes.csv"); //Open the file to output the data into
+    if(!outputFile.is_open()) {
+        cerr << "Error - could not open file" << endl;
+    }
+    //Print the headers for the output file
+    outputFile << ",Merge Sort,Quick Sort,Comb Sort,Shell Sort,Heap Sort,Algorithm Library Sort,Radix Sort (Radix=10)";
+    outputFile << ",Radix Sort (Radix=100),Radix Sort (Radix=1000),Radix Sort (Radix=10000),Radix Sort (Radix=10 & Max=1000)";
+    outputFile << ",Radix Sort (Radix=100 & Max=1000),Radix Sort (Radix=100 & Max=1000),Radix Sort (Radix=10000 & Max=1000)";
+    outputFile << ",Count Sort,Count Sort (Max=1000),Bucket Sort";
 
     //Iterate through each sorting algortihm
     for(int i = 0; i < numArrays; i++) {
-        currentArray = getRandomArray(arraySizes[i]);
+        size = arraySizes[i];
+        outputFile << "\n" << size << ",";
+        int *array = getRandomArray(arraySizes[i]);
         /* Call and time each function, exporting the result to a csv file */
+        chrono::time_point<std::chrono::high_resolution_clock> start = startTimer();
+        mergeSort(array, size);
+        auto timeElasped = endTimer(start);
+        outputFile << timeElasped << ",";
     } 
     
 }
@@ -69,6 +85,7 @@ int getNumArrays() {
             valid = true;
         }
     }
+    return numArrays;
 }
 
 
@@ -90,6 +107,7 @@ int *getSizes(int numOfArrays) {
         }
         *(sizeArr+i) = size;
     }
+    return sizeArr;
 }
 
 
@@ -104,16 +122,17 @@ int *getRandomArray(int size) {
 }
 
 
-auto startTimer() {
+chrono::time_point<std::chrono::high_resolution_clock> startTimer() {
     auto start = chrono::high_resolution_clock::now();
     return start;
 }
 
 
-auto endTimer(double start) {
-    auto end = chrono:high_resolution_clock::now();
-    auto timeElasped = chrono::duration_cast<chrono::microseconds>(end-start);
-    return (timeElasped*1000000); //convert to seconds
+long endTimer(chrono::time_point<std::chrono::high_resolution_clock> start) {
+    auto end = std::chrono::high_resolution_clock::now();
+    auto timeElasped = std::chrono::duration_cast<chrono::microseconds>(end-start);
+    timeElasped = timeElasped*1000000; //convert to seconds
+    return timeElasped.count();
 }
 
 //Given an array, will apply bounds to it (change the numbers so it only falls within 0 and the bound)
@@ -121,4 +140,5 @@ int *applyBounds(int *array, int arraySize, int bounds) {
     for(int i = 0; i < arraySize; i++) {
         array[i] = array[i]%(bounds);
     }
+    return array;
 }
